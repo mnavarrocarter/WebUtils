c874b4769f8939a7feb9220b0d50d1b3a8769fe7
Matías Navarro Carter <mnavarrocarter@gmail.com>
2017-08-23 18:28:53 -0300

    Removed tar.gz

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 debuild --no-tgz-check -us -uc && debuild clean
 mv ../*.deb dist/
-mv ../*.changes dist/
-mv ../*.build dist/
-mv ../*.dsc dist/
-mv ../*.tar.gz dist/
+rm ../*.changes
+rm ../*.build
+rm ../*.dsc
+rm ../*.tar.gz
